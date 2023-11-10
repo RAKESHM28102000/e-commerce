2bd6397fec9ee35dcf5a7a241a0095e8b4bf0fb4
RAKESHM28102000 <mrakeshmrakesh885@gmail.com>
2023-11-10 14:55:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// alert("welcome");
+
+document.getElementById("toggle").addEventListener('click',function(){
+
+    document.getElementById("toggle-menu").style.display="block"
+
+})
+document.getElementById('btnclose').addEventListener('click',function(){
+    document.getElementById("toggle-menu").style.display="none";
+})
+document.getElementById("toggle").addEventListener('click',function(){
+
+    document.getElementById("toggle-menu").style.display="none"
+
+})
